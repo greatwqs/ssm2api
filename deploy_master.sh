15d33ef813ced72d53f6d52130aee71d4d3fc7a3
wangqingsong <greatwqs@qq.com>
2018-03-04 16:46:27 +0800

    更新部署脚本

--- deploy_master.sh
+++ deploy_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-TOMCAT_HOME="/home/web/apache-tomcat-8.5.28"
+TOMCAT_HOME="/home/web/apache-tomcat-8.5.28-ssm2api"
 JAVA_HOME="/usr/lib/jvm/java-8-oracle"
 MAVEN_HOME="/usr/maven/apache-maven-3.5.2"
-PROJECT_HOME="/home/web/sourcecode/moon2api"
+PROJECT_HOME="/home/web/sourcecode/ssm2api"
 DEPLOY_HOME="/home/web/deployment"
 TIME=$(date +%Y%m%d_%H%M%S)
 
@@ -24,22 +24,22 @@ $MAVEN_HOME/bin/mvn clean install -P production
 
 echo ">> stop tomcat..."
 # sh $TOMCAT_HOME/bin/shutdown.sh
-kill -9 `ps -ef | grep java | grep web | grep tomcat | grep moon2api | awk ' BEGIN { FS = " " } { print $2 } '`
+kill -9 `ps -ef | grep java | grep web | grep tomcat | grep ssm2api | awk ' BEGIN { FS = " " } { print $2 } '`
 
 echo ">> move work copy.."
 cd $DEPLOY_HOME
 rm -rf $DEPLOY_HOME/*
-mkdir moon2api
-cd moon2api
+mkdir ssm2api
+cd ssm2api
 $JAVA_HOME/bin/jar -xvf $PROJECT_HOME/ssm2api-api/target/ssm2api.war
 rm -f WEB-INF/lib/jsp-api-*.jar
 rm -f WEB-INF/lib/servlet-api-*.jar
 
 #beckup logs and clean
 echo ">> beckup & clean all logs..."
-cp -r  $TOMCAT_HOME/moon2Logs $TOMCAT_HOME/moon2Logs_$TIME
+cp -r  $TOMCAT_HOME/ssmLogs $TOMCAT_HOME/ssmLogs_$TIME
 cp -r  $TOMCAT_HOME/logs      $TOMCAT_HOME/logs_$TIME
-rm -rf $TOMCAT_HOME/moon2Logs/*
+rm -rf $TOMCAT_HOME/ssmLogs/*
 rm -rf $TOMCAT_HOME/logs/*
 
 echo ">> start tomcat..."
